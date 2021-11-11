389e00cbc1b9e29b5ca77b98d281250e431199ba
erjillsison <erjillsison@gmail.com>
2021-11-11 10:54:33 +0800

    add nested package in a dir

--- dir/somefile.go
+++ dir/somefile.go
@@ -1,7 +1,11 @@
 package dir
 
-import "github.com/erjillsison/testgo"
+import (
+	"github.com/erjillsison/testgo"
+	"github.com/erjillsison/testgo2/dironly/packageindir"
+)
 
 func SomeFunc() {
 	testgo.SomeFunc2()
+	packageindir.Func()
 }
